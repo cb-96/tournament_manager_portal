e6247da0ca1a7274d784e1be093c821321435f69
cb-96 <cedric.bouckaert@hotmail.com>
2025-07-24 20:09:40 +0000

    Debugging

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Tournament Manager Portal",
+    'version': "18.0",
+    'author': "Cedric Bouckaert",
+    'category': "Website",
+    'website': "http://192.168.178.68:8069",
+    'summary': "Let team managers create players and lineups for matches via Odoo Website Portal.",
+    'depends': ['website', 'portal'],
+    'data': [
+        'security/ir.model.access.csv',
+	'views/portal_views.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from . import club
+from . import category
+from . import team
+from . import player
+from . import membership
+from . import match
+from . import lineup
