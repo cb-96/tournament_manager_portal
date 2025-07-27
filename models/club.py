1908c6cbbf838afdc946b1415f5ec71dbfd75874
Cedric <cedric.bouckaert@hotmail.com>
2025-07-27 11:41:55 +0200

    Typo

--- models/club.py
+++ models/club.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    is_club_managre = fields.Boolean(string='Is Club Manager', default=False)
+    is_club_manager = fields.Boolean(string='Is Club Manager', default=False)
 
 class TournamentClub(models.Model):
     #_inherit = 'res.partner'
